c59c5d3fa53829c8bc7028dfb28e8fb4e5e0bcc4
ryanquek22 <112756792+ryanquek22@users.noreply.github.com>
2023-06-30 02:38:35 +0800

    logging capabilities

--- main.py
+++ main.py
@@ -2,6 +2,29 @@ from pygooglenews import GoogleNews
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import pandas as pd
 import math
+import logging
+import logging.handlers
+import os
+import numpy as np
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger_file_handler = logging.handlers.RotatingFileHandler(
+    "status.log",
+    maxBytes=1024*1024,
+    backupCount=1,
+    encoding="utf8",
+)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger_file_handler.setFormatter(formatter)
+logger.addHandler(logger_file_handler)
+
+try:
+    SOME_SECRET = os.environ["SOME_SECRET"]
+except KeyError:
+    SOME_SECRET = "Token not available"
+    
+
 
 gn = GoogleNews()
 
@@ -35,9 +58,10 @@ for i in range(len(titles)):
 
 df['overall'] = overall
 
-df.to_csv('test.csv')
+average_score = np.mean(df['overall'])
+#df.to_csv('test.csv')
 
-#print("\n")
-#print(df)
+logger.info(f"Token value: {SOME_SECRET}")
+logger.info(f"Average Vader Sentiment of 100 Mircrosoft articles over the past 6 months is {average_score} ")
 
 
